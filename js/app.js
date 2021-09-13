4b428d7b21d9bf7cba81d1facfb1a6d6c43ac8e2
EASHOVON <ashrafuzzaman.shovon.786@gmail.com>
2021-09-13 16:45:42 +0600

    After Point added two number

--- js/app.js
+++ js/app.js
@@ -13,6 +13,8 @@ const showProducts = (products) =>
   {
     // Second Bug //
     const image = product.image;
+    const prices = product.price;
+    const price = prices.toFixed(2);
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
@@ -21,7 +23,7 @@ const showProducts = (products) =>
       </div>
       <h3>${ product.title }</h3>
       <p>Category: ${ product.category }</p>
-      <h2>Price: $ ${ product.price }</h2>
+      <h2>Price: $ ${ price }</h2>
       <button onclick="addToCart(${ product.id },${ product.price })" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
       `;
@@ -42,7 +44,7 @@ const addToCart = (id, price) =>
 const getInputValue = (id) =>
 {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element); // ParseInt to ParseFloat
   return converted;
 };
 
@@ -52,13 +54,13 @@ const updatePrice = (id, value) =>
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = total.toFixed(2); //After Point added two number
 };
 
 // set innerText function
 const setInnerText = (id, value) =>
 {
-  document.getElementById(id).innerText = Math.round(value);
+  document.getElementById(id).innerText = value.toFixed(2); //After Point added two number
 };
 
 // update delivery charge and total Tax
@@ -88,6 +90,6 @@ const updateTotal = () =>
   const grandTotal =
     getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+  document.getElementById("total").innerText = grandTotal.toFixed(2);//After Point added two number 
 };
 loadProducts();
